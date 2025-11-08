8756e85bbee171e765ad87adbb331b42489b8aa0
Declan <declan@linux-desktop.lan>
2025-11-08 14:52:21 -0600

    Fix environment variable usage for PUBLIC_API_ORIGIN in server code

--- web/src/routes/(protected)/app/+page.server.ts
+++ web/src/routes/(protected)/app/+page.server.ts
@@ -1,14 +1,13 @@
 import type { Actions, PageServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 import { env } from "$env/dynamic/private";
-import { PUBLIC_API_ORIGIN } from "$env/dynamic/public";
 import { createApiClient, type AuthStatus } from "$lib/apiClient";
 
 const API_URL_INTERNAL = (env.API_ORIGIN ?? "http://localhost:8000").replace(
   /\/+$/,
   "",
 );
-const API_URL_EXTERNAL = (PUBLIC_API_ORIGIN ?? API_URL_INTERNAL).replace(
+const API_URL_EXTERNAL = (env.PUBLIC_API_ORIGIN ?? API_URL_INTERNAL).replace(
   /\/+$/,
   "",
 );
